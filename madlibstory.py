551782c3b67070b4771d174e3181131eae451ec3
sumitdwivedi0640 <112279412+sumitdwivedi0640@users.noreply.github.com>
2023-08-09 20:39:52 +0530

    Add files via upload

---
+++ madlibstory.py
@@ -0,0 +1,10 @@
+animals= input("Enter a animal name : ")
+profession = input("Enter a profession name: ")
+cloth = input("Enter a piece of cloth name: ")
+things = input("Enter a thing name: ")
+name= input("Enter a name: ")
+place = input("Enter a place name: ")
+verb = input("Enter a verb in ing form: ")
+food = input("Food name: ")
+print("Say " + food + " , the photographer said as the camera flashed " + name + " and I had gone to " + place +" to get our photos taken on my birthday. The first photo we really wanted was a picture of us dressed as " + animals + " pretending to be a " + profession + ". when we saw the second photo, it was exactly what I wanted. We both looked like " + things + " wearing " + cloth + " and " + verb +" --exactly what I had in mind")
+
